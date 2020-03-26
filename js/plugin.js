0e8dfc07c186851ff5bdc0635abb215b6d8a737f
tnmthu <tnmthu.maria98@gmail.com>
2020-03-26 18:03:21 +0700

    [WIP]

--- js/plugin.js
+++ js/plugin.js
@@ -1,6 +1,5 @@
 (function($) {
   $.fn.table = function(metadata) {
-    // this fucking shit
     var stripQuotes = function(s) {
       return s.replace(/['"]+\g/, '');
     }
@@ -18,8 +17,6 @@
       type.push(item.type);
     });
 
-    const tableData = data;
-
     $(this).empty();
     $(this).append(`
       <table style="width: ${stripQuotes(width)}">
@@ -30,7 +27,7 @@
           }).join("")}
         </thead>
         <tbody>
-          ${tableData.map(function(item) {
+          ${data.map(function(item) {
             return `
             <tr id="${item["id"]}">
               <td>
@@ -58,7 +55,9 @@
           </ul>
         </nav>
       </div>
-    `)
+    `);
+ 
+
 
       
     // pagination
@@ -68,16 +67,17 @@
     let totalRows = $("table tbody tr").length;
 
     var limitPaging = function() {
-      if($('.pagination li').length > 7) {
+      if($('.pagination li').length > 10) {
         if ($('.pagination li.active').attr('data-page') <= 3) {
           $('.pagination li:gt(5)').hide();
           $('.pagination li:lt(5)').show();
+          $('.pagination [data-page="next"]').show();
         }
         if ($('.pagination li.active').attr('data-page') > 3) {
           $('.pagination li:gt(0)').hide();
           $('.pagination [data-page="next"]').show();
-          let x = parseInt($('.pagination li.active').attri('data-page')) - 2;
-          let y = parseInt($('.pagination li.active').attri('data-page')) + 2;
+          let x = parseInt($('.pagination li.active').attr('data-page')) - 2;
+          let y = parseInt($('.pagination li.active').attr('data-page')) + 2;
           for (let i = x; i <= y; i++) {
             $(`.pagination [data-page="${i}"]`).show();
           }
@@ -145,6 +145,8 @@
       const idx = $(this).attr("data-idx");
       let rows = $(this).closest("table").find("tbody tr");
       let asc = $(this).attr("data-asc");
+      $(this).siblings().removeAttr("data-asc");
+
       if ($(this).children("input:checkbox").length) {
         return;
       }
@@ -178,8 +180,116 @@
           $(this).show();
         }
       });
+      limitPaging();
     });
-  
-
   }
 }(jQuery));
+
+// var Pagination = {
+//   code: '',
+
+//   Add: function(start, end) {
+//     for (let i = start; i < end; i++) 
+//     Pagination.code += `<a>${i}</a>`;
+//   },
+
+//   First: function() {
+//     Pagination.code += `<a>1</a><i>...</i>`;
+//   },
+
+//   Last: function() {
+//     Pagination.code += `<i>...</i><a>${Pagination.limit}</a>`;
+//   },
+
+//   Buttons: function(e) {
+//     e.on("click", "a:first-child", function() {
+//       Pagination.Prev();
+//     });
+//     e.on("click", "a:last-child", function() {
+//       Pagination.Next();
+//     });
+//   },
+
+//   Click: function() {
+//     Pagination.page = parseInt($(this).html());
+//     Pagination.Start();
+//   },
+
+//   Prev: function() {
+//     Pagination.page--;
+//     if (Pagination.page < 1) {
+//       Pagination.page = 1;
+//     }
+//     Pagination.Start();
+//   },
+
+//   Next: function() {
+//     Pagination.page++;
+//     if (Pagination.page > Pagination.limit) {
+//       Pagination.page = Pagination.size;
+//     }
+//     Pagination.Start();
+//   },
+
+//   Extend: function(pagData) {
+//     Pagination.page = 1;
+//     Pagination.limit = pagData.limit || 300;
+//     Pagination.step = pagData.step || 2;
+//   },
+
+//   Create: function(e) {
+//     e.append(`
+//     <a>&#9668;</a>
+//     <span></span>
+//     <a>&#9658;</a>
+//     `);
+//     Pagination.e = e.find("span");
+//     Pagination.Buttons(Pagination.e);
+//   },
+
+//   Start: function(index) {
+//     if (Pagination.limit < Pagination.step * 2 + 6) {
+//       Pagination.Add(1, Pagination.limit + 1);
+//     } else if (Pagination.page < Pagination.step * 2 + 1) {
+//       Pagination.Add(1, Pagination.step * 2 + 4);
+//       Pagination.Last();
+//     } else if (Pagination.page > Pagination.limit - Pagination.step * 2) {
+//       Pagination.First();
+//       Pagination.Add(Pagination.limit - Pagination.step * 2 - 2, Pagination.limit + 1);
+//     } else {
+//       Pagination.First();
+//       Pagination.Add(Pagination.page - Pagination.step, Pagination.page + Pagination.step + 1);
+//       Pagination.Last()
+//     }
+
+//     Pagination.Finish();
+//   },
+
+//   Finish: function() {
+//     Pagination.e.html(Pagination.code);
+//     Pagination.code = "";
+//     Pagination.Bind();
+//   },
+
+//   Bind: function() {
+//     let a = Pagination.e.find("a");
+//     for (let i = 0; i < a.length; i++) {
+//       if (parseInt(a.eq(i).html()) === Pagination.page) {
+//         a.eq(i).addClass("current");
+//       }
+//       a.eq(i).on("click", function() {
+//         Pagination.Click();
+//       });
+//     }
+//   },
+
+//   Init: function(e, pagData, index) {
+//     Pagination.Extend(pagData);
+//     Pagination.Create(e);
+//     Pagination.Start(index);
+//   }
+// };
+
+// (function() {
+// Pagination.Init($('#pagination'), pagination, 1);
+// })();
